b744b2df4f36dbe7b7a3a3b44f2b7292fb564ea7
Johan Berdat <jojolebarjos@gmail.com>
2020-03-28 14:21:02 +0100

    Add stub

---
+++ setup.py
@@ -0,0 +1,53 @@
+
+from setuptools import find_packages, setup
+
+
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+
+    name = 'kprototypes',
+    version = '0.1.0',
+    packages = find_packages(),
+
+    author = 'Johan Berdat',
+    author_email = 'jojolebarjos@gmail.com',
+    license = 'MIT',
+
+    url = 'https://gitlab.com/jojolebarjos/kprototypes',
+
+    description = 'k-prototypes for numerical and categorical clustering',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+
+    keywords = [
+        'clustering',
+        'k-prototypes',
+        'k-means',
+    ],
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+
+    install_requires = [
+        'numba',
+        'numpy',
+    ]
+
+)
